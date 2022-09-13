e58c412cd78981a1e8d5992e62b5061e314dccd1
Fabio <fabio@screendragon.com>
2022-09-13 12:25:58 +0200

    update config.ps1 with dummy data

--- config.ps1
+++ config.ps1
@@ -1,23 +1,23 @@
 
 # SMTP config
 
-$env:smtpServer = ''
+$env:smtpServer = 'smtp_server_here'
 
 $env:smtpServerport = 587
 
-$env:emailSmtpUser = ''
+$env:emailSmtpUser = 'user_here'
 
-$env:emailSmtpPass = ''
+$env:emailSmtpPass = 'password_here'
 
 # Amazon AWS SES configSet
 
-$env:configSet = ''
+$env:configSet = 'config_set_here'
 
 # Email config
 
-$env:from = ''
+$env:from = 'yourEmail.com'
 
-$env:to = ''
+$env:to = 'myFriend@gmail.com,myOtherFriend@hisDomain.net'
 
 $env:subject = 'Amazon SES test (SMTP interface accessed using Powershell)'
 
